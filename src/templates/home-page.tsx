8c085c38e620810bba8dcc8fe215704468415bf6
Stephen Zhou <hi@hyoban.cc>
2024-03-01 13:45:49 +0800

    chore: use env

--- src/templates/home-page.tsx
+++ src/templates/home-page.tsx
@@ -1,12 +1,14 @@
-import { Link } from 'waku';
+import { Link, getEnv } from 'waku';
 
 import { Counter } from '../components/counter.js';
 
 export const HomePage = async () => {
   const data = await getData();
+  const someEnv = getEnv('SOME_ENV');
 
   return (
     <div>
+      <h1>someEnv: {someEnv}</h1>
       <title>{data.title}</title>
       <h1 className="text-4xl font-bold tracking-tight">{data.headline}</h1>
       <p>{data.body}</p>
